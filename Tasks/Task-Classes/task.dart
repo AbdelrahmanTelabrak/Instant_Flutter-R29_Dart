b4a5d90dc4a9e227a37588ed073a73a10bd6c9bb
AbdelrahmanTelabrak <abdelrahmantelabrak@gmail.com>
2024-01-21 18:17:13 +0200

    Text Task

--- Tasks/Task-Classes/task.dart
+++ Tasks/Task-Classes/task.dart
@@ -26,42 +26,52 @@ create a Text class contains:
 ***************/
 
 void main(List<String> args) {
-  TextAlign align = TextAlign('right');
-  TextStyle style = TextStyle(color: 'red', fontSize: 14, fontFamily: 'Poppins');
-  Text text = Text('welcome', style: style, align: align);
+  // TextAlign align = TextAlign('right');
+  // TextStyle style = TextStyle(color: 'red', fontSize: 14, fontFamily: 'Poppins');
+  Text text = Text(
+    'welcome',
+    style: TextStyle(
+    color: 'red', 
+    fontSize: 14, 
+    fontFamily: 'Poppins'
+    ),
+    align: TextAlign('right')
+  );
   text.displayInfo();
 }
 
-class Text{
+class Text {
   String data;
   TextStyle style;
   TextAlign align;
 
   Text(this.data, {required this.style, required this.align});
 
-  void displayInfo(){
-    print('data: ${this.data},\nstyle: ${this.style.displayInfo()}, \nalign: ${this.align.displayInfo()}');
+  void displayInfo() {
+    print(
+        'data: ${this.data},\nstyle: ${this.style.displayInfo()}, \nalign: ${this.align.displayInfo()}');
   }
 }
 
-class TextStyle{
+class TextStyle {
   String color;
   int fontSize;
   String fontFamily;
 
-  TextStyle({required this.color, required this.fontSize, required this.fontFamily});
+  TextStyle(
+      {required this.color, required this.fontSize, required this.fontFamily});
 
-  String displayInfo(){
+  String displayInfo() {
     return ('color: ${this.color}, size: ${this.fontSize}, fontFamily: \'${this.fontFamily}\'');
   }
 }
 
-class TextAlign{
+class TextAlign {
   String? align;
 
   TextAlign(this.align);
 
-  String displayInfo(){
+  String displayInfo() {
     return this.align!;
   }
 }
